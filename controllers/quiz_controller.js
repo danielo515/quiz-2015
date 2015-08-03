3a2b93bdf0006b7a64a4e540bd3997e90c1d0732
danielo515 <rdanielo@gmail.com>
2015-08-03 20:52:40 +0200

    Categorización de las preguntas por temas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -37,7 +37,7 @@ exports.answer = function(req, res) {
 // GET /quizes/new
 exports.new = function(req, res) {
   var quiz = models.Quiz.build(
-  {pregunta: "Pregunta", respuesta: "Respuesta"}
+  {pregunta: "", respuesta: ""}
   );
   res.render('quizes/new', {quiz: quiz});
 };
@@ -53,7 +53,7 @@ exports.create = function(req, res) {
           res.render('quizes/new', {quiz: quiz, errors: err.errors});
         } else {
           quiz // save: guarda en DB campos pregunta y respuesta de quiz
-          .save({fields: ["pregunta", "respuesta"]})
+          .save({fields: ["pregunta", "respuesta","tema"]})
           .then( function(){ res.redirect('/quizes')})
         }      // res.redirect: Redirección HTTP a lista de preguntas
       }
