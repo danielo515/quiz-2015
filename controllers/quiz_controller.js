4c43982b9f2471822d56835925f5ce0c489a19a8
danielo515 <rdanielo@gmail.com>
2015-08-03 09:24:41 +0200

    Creación de preguntas y formulario

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -30,7 +30,25 @@ exports.answer = function(req, res) {
   if (req.query.respuesta.toLowerCase() === req.quiz.respuesta.toLowerCase()) {
     resultado="Correcto"
         }
-    res.render('quizes/answer', { quiz: quiz, respuesta: resultado });
+    res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado });
+};
+
+
+// GET /quizes/new
+exports.new = function(req, res) {
+  var quiz = models.Quiz.build(
+  {pregunta: "Pregunta", respuesta: "Respuesta"}
+  );
+  res.render('quizes/new', {quiz: quiz});
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+  var quiz = models.Quiz.build( req.body.quiz );
+// guarda en DB los campos pregunta y respuesta de quiz
+  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+    res.redirect('/quizes');
+  })   // res.redirect: Redirección HTTP a lista de preguntas
 };
 
 exports.author=function(req, res) {
