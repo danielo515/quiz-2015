566d3e5fe841bc4999b166d8de23fc4bf28dd60c
danielo515 <rdanielo@gmail.com>
2015-08-17 16:23:51 +0200

    Moderación de comentarios

--- models/comment.js
+++ models/comment.js
@@ -6,7 +6,11 @@ module.exports = function(sequelize, DataTypes) {
       { texto: {
           type: DataTypes.STRING,
           validate: { notEmpty: {msg: "-> Falta texto del comentario"}}
-        }
+        },
+        publicado: {
+      	type: DataTypes.BOOLEAN,
+      	defaultValue: false
+       }
       }
     );
 }
--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 router.param('quizId', quizController.load);  // autoload :quizId
+router.param('commentId', commentController.load);  // autoload :commentId
+
 
 // Definición de rutas de sesion
 router.get('/login',  sessionController.new);     // formulario login
@@ -28,5 +30,7 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizCont
 
 router.get('/quizes/:quizId(\\d+)/comments/new',            commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',              commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+	                                    sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
